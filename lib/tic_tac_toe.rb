16f30f7b606af32a0735cda8ec8b2838d23c699a
Chao Hsuan Chi <allen0413@gmail.com>
2017-10-27 16:14:53 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -13,6 +13,14 @@ class TicTacToe
     [0,4,8],
     [6,4,2]
   ]
+
+  def display_board
+    puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
+    puts "-----------"
+    puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
+    puts "-----------"
+    puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
+  end
   
   def current_player
     turn_count % 2 == 0 ? "X" : "O"
@@ -22,11 +30,5 @@ class TicTacToe
     @board.count{|token| token == "X" || token == "O"}
   end
 
-  def display_board
-    puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
-    puts "-----------"
-    puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
-    puts "-----------"
-    puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
-  end
+
 end
